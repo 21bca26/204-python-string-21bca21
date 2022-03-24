8d72454574173d7c6eb47799b54e7c6f0592927a
21bca21 <96718294+21bca21@users.noreply.github.com>
2022-03-24 11:19:20 +0530

    Add files via upload

---
+++ solution/stringeven.py
@@ -0,0 +1,17 @@
+#write a script to enter 5 string in a list and check and print string who's lenth even number of character(without any string function)
+def createlist():
+    l=[]
+    for i in range(5):
+        s=input("enter string:")
+        l.append(s)
+def string(s):
+    count=0
+    for i in l:
+        for j in i:
+            if len(string)%2==0:
+                print(string(s))
+                count+=1
+            return(count)
+createlist()
+ans=string(s)
+print(ans)
